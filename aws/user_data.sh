f85417710230a4f62942e7a80c6a6c54c4dfb4f8
Pedro Arenas <pedroars99@icloud.com>
2024-01-27 00:27:06 +0100

    Deploy Instance with mosquitto

---
+++ aws/user_data.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo apt-add-repository --yes ppa:mosquitto-dev/mosquitto-ppa
+sudo apt-get update
+
+sudo apt-get --assume-yes install mosquitto mosquitto-clients
+
+cat << EOS
+listener 1883
+allow_anonymous true
+EOS >> mosquitto.conf
+
+mosquitto -c mosquitto.conf -d -p 1883
