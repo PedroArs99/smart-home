cb2a89c697515417c3ac1a2053c7961e3369b392
Pedro Arenas <pedroars99@icloud.com>
2024-01-27 19:47:45 +0100

    Create new-home-control

---
+++ new-home-control/App.js
@@ -0,0 +1,33 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View, Image } from "react-native";
+
+const PlaceholderImage = require("./assets/images/background-image.png");
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <Image source={PlaceholderImage} style={styles.image} />
+      </View>
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  image: {
+    borderRadius: 18,
+    width: 320,
+    height: 440,
+  },
+  imageContainer: {
+    flex: 1,
+  },
+});
