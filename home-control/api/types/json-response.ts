7de4e26632f88342b05e3a59639c58f7f26788cb
Pedro Arenas <pedroars99@icloud.com>
2024-03-11 00:36:32 +0100

    Fix Cors

--- home-control/api/types/json-response.ts
+++ home-control/api/types/json-response.ts
@@ -2,6 +2,7 @@ export function jsonResponse(statusCode: number, body: any) {
   return {
     headers: {
       "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true,
       "Content-Type": "application/json; charset=utf-8",
     },
     body: JSON.stringify(body),
